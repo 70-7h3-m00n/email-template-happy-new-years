c849ae75409b3190e0504c68982f092c4c81a587
novailoveyou <novailoveyoux@gmail.com>
2021-12-31 12:17:33 +0300

    updated export template name

--- makeEmailTemplate.js
+++ makeEmailTemplate.js
@@ -20,7 +20,7 @@ fs.readFile('./out/index.html', 'utf8', (err, data) => {
       .replace(/(\.jpg\"\))/g, ".jpg')")
       .replace(/(\.png\"\))/g, ".png')")
 
-  fs.writeFile('../halloween-template.html', content, err => {
+  fs.writeFile('../template-happy-new-years.html', content, err => {
     if (err) {
       console.error(err)
       return
